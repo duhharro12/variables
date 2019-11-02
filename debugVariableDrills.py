aa16f28272404c54fb70f511be94221c5ff61c40
duhharro12 <55327251+duhharro12@users.noreply.github.com>
2019-11-02 10:31:52 -0500

    Create debugVariableDrills.py

---
+++ debugVariableDrills.py
@@ -0,0 +1,109 @@
+'''
+For this assignment you should look at the variables create below and find the error.
+For each task, there will be one error that you must find and correct.
+Sometimes there will be an explanation of the problem and/or tips that can help you
+complete the tasks.
+EXAMPLE TASK:
+'''
+#EX)
+#Broken:
+#name == "Michael"
+#print(name)
+#Correct:
+name = "Michael"
+print(name)
+'''
+END EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1)
+#Broken: 2 equal signs
+#name == "your name"
+#print(name) 
+#Correct: "#name = " your name""
+
+#2)
+#Broken: There isn't a Quotation mark at the end
+#x = "your name
+#print(x)
+#Correct: "your name"
+
+#3)
+#Broken: the word is misspelled
+#yay = "yay"
+#print(aya)
+#PROBLEM: The variable is created correctly, but it won't print.
+#TIP: The variable isn't the problem
+#Correct: "#print(yay)
+
+#4)
+#Broken: there is 2 equal signs
+#y == 4
+#print(y)
+#Correct: "#y = 4"
+
+#5)
+#Broken: thre is 2 equal signs
+#m == 5
+#print(m)
+#Correct:"#m = 5"
+
+'''
+For the next section, there will be a variable create correctly. Your job will
+be to write what kind of variable it is.
+EXAMPLE:
+'''
+#EX)
+#name = "michael"
+#ANSWER: string
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+#1)
+#x = 1.01
+#ANSWER: float
+
+#2)
+#y = 2
+#ANSWER: integer
+
+#3)
+#a = True
+#ANSWER: boolan
+
+#4)
+#b = 1
+#ANSWER: integer
+
+#5)
+#c = "string"
+#ANSWER: string
+
+#6)
+#d = False
+#ANSWER: boolan
+
+#7)
+#e = ["a", "b", "c"]
+#ANSWER: list
+
+#8)
+#f = [1, 2, 3]
+#ANSWER: list
+
+#9)
+#g = "nope"
+#ANSWER: string
+
+#10)
+#h = 1.24
+#ANSWER: float
